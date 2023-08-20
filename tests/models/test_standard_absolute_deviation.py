44d4ff6c19da336664229f57a2d0809ae9795b71
Justine1010 <justinehui1010@gmail.com>
2023-08-20 22:39:43 +0800

    add square_least in standard_absolute_deviation

--- tests/models/test_standard_absolute_deviation.py
+++ tests/models/test_standard_absolute_deviation.py
@@ -4,6 +4,7 @@ def test_standard_absolute_deviation():
     import numpy as np
     from numpy.testing import assert_raises
     from pysad.utils import fix_seed
+    from scipy.optimize import least_squares
 
     fix_seed(61)
     X = np.random.rand(150, 1)
@@ -12,6 +13,7 @@ def test_standard_absolute_deviation():
     model = model.fit(X)
     y_pred = model.score(X)
     assert y_pred.shape == (X.shape[0],)
+    assert y_pred.choose == least_squares(y_pred)
 
     model = StandardAbsoluteDeviation(substracted_statistic="median")
     model = model.fit(X)
